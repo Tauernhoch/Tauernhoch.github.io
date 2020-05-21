c3334afbd3e064cee2ab778d07d8bab5e825e9dc
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.fritz.box>
2020-05-21 16:56:08 +0200

    Plugins inatalliert

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-let startLayer = L.tileLayer.provider("BasemapAT.grau");
+let mapdiv = document.querySelector("#map");
 
 let map = L.map("map", {
     center: [47.0748663672 , 12.695247219 ],
     zoom: 8,
     layers: [
-        startLayer
-    ]
-});
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+             maxZoom: 17,
+             attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+         })
+     ]
+ }); 
 
 
