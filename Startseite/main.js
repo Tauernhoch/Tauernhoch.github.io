bf6309c9cfcd2104d3f95400c23d050a08b93f80
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.fritz.box>
2020-05-21 17:03:52 +0200

    beautify code

--- Startseite/main.js
+++ Startseite/main.js
@@ -1,14 +1,12 @@
 let mapdiv = document.querySelector("#map");
 
 let map = L.map("map", {
-    center: [47.0748663672 , 12.695247219 ],
+    center: [47.0748663672, 12.695247219],
     zoom: 8,
     layers: [
         L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-             maxZoom: 17,
-             attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-         })
-     ]
- }); 
-
-
+            maxZoom: 17,
+            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+        })
+    ]
+});
