1a8ecf986082a0c97cec634c667eb68537023060
Marco Hilpert <marcohilpert@Marcos-MacBook-Air.local>
2020-06-15 20:51:36 +0200

    Versuch Wasser Layer zu erstellen

--- Startseite/wasser.js
+++ Startseite/wasser.js
@@ -1,48 +1,30 @@
-    {
-    "type": "FeatureCollection",
-    "name": "geo_js",
-    "crs": {
-        "type": "name",
-        "properties": {
-            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
-        }
-    },
-    {
-        "type": "Feature",
-        "properties": {
-            "OBJECTID": 76,
-            "NAME": "Schleierfall",
-            "SEEHOEHE": null,
-            "URL_INTERN": " ",
-            "URL_EXTERN": " ",
-            "POI_TYP": 33,
-            "LAND": "Tirol",
-            "BILDUNG": 0,
-            "JARPRO15": 1
-            
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [12.467115964876346, 47.133193158585684]
-        }
-    },
-    {
-        "type": "Feature",
-        "properties": {
-            "OBJECTID": 524,
-            "NAME": "Mountainbikeroute Zettersfeldrunde",
-            "SEEHOEHE": null,
-            "URL_INTERN": " ",
-            "URL_EXTERN": " ",
-            "POI_TYP": 32,
-            "LAND": "Tirol",
-            "BILDUNG": 0,
-            "JARPRO15": 1
-        },
-        "geometry": {
-            "type": "Point",
-            "coordinates": [12.804435057998742, 46.853466675272237]
-        }
-    }
-]
+   const Wasser = {
+       "type": "FeatureCollection",
+       "name": "geo_js",
+       "crs": {
+           "type": "name",
+           "properties": {
+               "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+           }
+       },
+       {
+           "type": "Feature",
+           "properties": {
+               "OBJECTID": 76,
+               "NAME": "Schleierfall",
+               "SEEHOEHE": null,
+               "URL_INTERN": " ",
+               "URL_EXTERN": " ",
+               "POI_TYP": 33,
+               "LAND": "Tirol",
+               "BILDUNG": 0,
+               "JARPRO15": 1
+
+           },
+           "geometry": {
+               "type": "Point",
+               "coordinates": [12.467115964876346, 47.133193158585684]
+           }
+       }
+   }
 }
